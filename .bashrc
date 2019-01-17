81477d2d9e5896095d9e0948e0a93ddd833839b1
Mohammed Elansary <mohammed.ansary@experteer.com>
2019-01-17 13:23:19 +0100

    Initial commit: Structure and git aliases

---
+++ .bashrc
@@ -0,0 +1,10 @@
+
+# mo_bash aliases
+if [ -f ~/.mo_bash/config/aliases ]; then
+    . ~/.mo_bash/config/aliases
+fi
+
+# mo_bash functions
+if [ -f ~/.mo_bash/config/functions ]; then
+    . ~/.mo_bash/config/functions
+fi
---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+# create backup dir if not already exists
+mkdir -p ~/.mo_bash/backups
+
+# backup ~/.bash_profile for mac then add the
+# content of the .bash_profile in this dir
+# to it
+if [ "${machine}" = "Mac" ]; then
+  cp ~/.bash_profile ~/.mo_bash/backups/.bash_profile
+  cat ~/.mo_bash/.bash_profile >> ~/.bash_profile
+fi
+
+# backup ~/.bashrc then add the content of
+# the .bashrc in this dir to it
+cp ~/.bashrc ~/.mo_bash/backups/.bashrc
+cat ~/.mo_bash/.bashrc >> ~/.bashrc
+
+source ~/.bashrc
